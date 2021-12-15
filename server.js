5db09a54063c1baa1e816b635181f05fdc9c0462
lejt <92820402+lejt@users.noreply.github.com>
2021-12-15 14:11:45 -0800

    base code for google OAuth and general server template

---
+++ server.js
@@ -0,0 +1,71 @@
+/* 
+modules to install:
+npm i, npm i mongoose, npm i dotenv, npm i express-session, npm i passport,
+npm i passport-google-oauth, npm i method-override
+*/
+
+
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var session = require('express-session');
+var passport = require('passport');
+var methodOverride = require('method-override');
+
+// first load your .env file
+require('dotenv').config();
+	
+// connect to the database with Mongoose
+require('./config/database');
+require('./config/passport');
+
+app.use(function (req, res, next) {
+  res.locals.user = req.user;
+  next();
+});
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(methodOverride('_method'));
+app.use(session({
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
