67555c0c88b67151260c38952581a14221d50b10
lejt <92820402+lejt@users.noreply.github.com>
2021-12-22 15:39:54 -0800

    fix modal

--- public/javascripts/popupForm_update.js
+++ public/javascripts/popupForm_update.js
@@ -1,34 +1,24 @@
 // Get the modal
-const modalAddDesc = document.getElementById("modal_addDesc");
 const modalUpdateDesc = document.getElementById("modal_updateDesc");
 
 // Get the button that opens the modal
-const addDescBtn = document.getElementById("addDescBtn");
 const updateDescBtn = document.getElementById("updateDescBtn");
 
 // Get the <span> element that closes the modal
-const span = document.getElementsByClassName("close1")[0];
-// const span2 = document.getElementsByClassName("closeUpdate")[0];
+const closeUpdate = document.getElementById("closeUpdate");
 
 // When the user clicks the button, open the modal
-if (addDescBtn) {
-    addDescBtn.onclick = function() {
-        modalAddDesc.style.display = "block";
-    }
-}
 if (updateDescBtn) {
     updateDescBtn.onclick = function() {
         modalUpdateDesc.style.display = "block";
     }
 }
-span.onclick = function() {
+closeUpdate.onclick = function() {
     modalUpdateDesc.style.display = "none";
 }
 
 window.onclick = function(event) {
-    if (event.target == modalAddDesc) {
-        modalAddDesc.style.display = "none";
-    } else if (event.target == modalUpdateDesc) {
+    if (event.target == modalUpdateDesc) {
         modalUpdateDesc.style.display = "none";
     }
 };
