a6fa139b63f80a1acdb5c2e3704b1c505a82f589
lejt <92820402+lejt@users.noreply.github.com>
2021-12-22 17:05:00 -0800

    create tab saving function

--- public/javascripts/tabs_script.js
+++ public/javascripts/tabs_script.js
@@ -1,5 +1,34 @@
+// USES LOCAL STORAGE TO SAVE CURRENT TAB OF PAGE
+if (!localStorage.getItem("currentId")) { 
+   currentIdValue = "page1";
+   localStorage.setItem("currentId", currentIdValue);
+};
+
+// console.log(localStorage.getItem("currentId"));
+
+page1 = document.getElementById("page1").id;
+page2 = document.getElementById("page2").id;
+page3 = document.getElementById("page3").id;
+page4 = document.getElementById("page4").id;
+page5 = document.getElementById("page5").id;
+
+tabs = [page1, page2, page3, page4, page5];
+
+for (let i=0; i<tabs.length; i++) {
+   if (tabs[i] == localStorage.getItem("currentId")) {
+      document.getElementById(tabs[i]).className="selected";
+      document.getElementById("page_content").innerHTML=document.getElementById(localStorage.getItem("currentId")+"_desc").innerHTML;
+   } else {
+      document.getElementById(tabs[i]).className="notselected";
+   }
+}
+
+// FUNCTION FOR CHANGING TABS ON SINGLE PAGE
 function change_tab(id) {
-   document.getElementById("page_content").innerHTML=document.getElementById(id+"_desc").innerHTML;
+   currentIdValue = id;
+   localStorage.setItem("currentId", currentIdValue);
+   
+   document.getElementById("page_content").innerHTML=document.getElementById(localStorage.getItem("currentId")+"_desc").innerHTML;
    document.getElementById("page1").className="notselected";
    document.getElementById("page2").className="notselected";
    document.getElementById("page3").className="notselected";
